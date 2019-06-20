86be3a9de8032903f91161f07e26b6033e7c60d6
syswu <wuheng@iscas.ac.cn>
2019-06-20 08:47:10 +0800

    update

--- src/main/java/com/github/kubesys/kubernetes/ExtendedKubernetesClient.java
+++ src/main/java/com/github/kubesys/kubernetes/ExtendedKubernetesClient.java
@@ -29,41 +29,41 @@ import io.fabric8.kubernetes.internal.KubernetesDeserializer;
 
 /**
  * @author wuheng@otcaix.iscas.ac.cn
- * @since  2019/5/1
+ * @since 2019/5/1
  *
- * This code is used to manage CustomResource's lifecycle,
- * such as VirtualMachine
+ *        This code is used to manage CustomResource's lifecycle, such as
+ *        VirtualMachine
  */
 public class ExtendedKubernetesClient extends DefaultKubernetesClient {
-	
-	public final static Logger m_logger = Logger.getLogger(ExtendedKubernetesClient.class.getName());
-	
-	public final static String TOKEN    = "/etc/kubernetes/admin.conf";
-	
-	public final static String PACKAGE  = ExtendedKubernetesClient.class.getPackage().getName();
-	
-	public final static String SUBPKG   = ".api.model.";
-	
-	public final static Map<String, CustomResourceDefinition> crds = 
-											new HashMap<String, CustomResourceDefinition>();
-	
-	public final static Map<String, MixedOperation> executors = 
-											new HashMap<String, MixedOperation>();
-	
-	protected void initCustomResources() throws Exception {
-		for(File conf : getConfigs()) {
-    		// it is not a configure
-    		if (!conf.getName().endsWith("conf")) {
-    			continue;
-    		}
-    		
-    		// register to kubernetes
-			Properties props = loadConfig(conf);
-			registerToKubernetes(props);
-			
-    	}
-	}
 
+	public final static Logger m_logger = Logger.getLogger(ExtendedKubernetesClient.class.getName());
+
+	public final static String TOKEN = "/etc/kubernetes/admin.conf";
+
+	public final static String PACKAGE = ExtendedKubernetesClient.class.getPackage().getName();
+
+	public final static String SUBPKG = ".api.model.";
+
+	public final static Map<String, CustomResourceDefinition> crds = new HashMap<String, CustomResourceDefinition>();
+
+	public final static Map<String, MixedOperation> executors = new HashMap<String, MixedOperation>();
+
+	protected void initCustomResources() throws Exception {
+//		for(File conf : getConfigs()) {
+//    		// it is not a configure
+//    		if (!conf.getName().endsWith("conf")) {
+//    			continue;
+//    		}
+//    		
+//    		
+//			
+//    	}
+		// register to kubernetes
+		Properties props =  new Properties();
+		props.load(getClass()
+				.getResourceAsStream("/VirtualMachine.conf"));
+		registerToKubernetes(props);
+	}
 
 	public ExtendedKubernetesClient(Config config) throws Exception {
 		super(config);
@@ -71,31 +71,25 @@ public class ExtendedKubernetesClient extends DefaultKubernetesClient {
 	}
 
 	/**
-     * @param props
-     * @throws Exception
-     */
-    protected void registerToKubernetes(Properties props) throws Exception {
-    	String name    = props.getProperty("PLURAL")
-				+ "." + props.getProperty("GROUP");
-		String kind    = props.getProperty("KIND");
-		String version = props.getProperty("GROUP") 
-						+ "/" + props.getProperty("VERSION");
-		
+	 * @param props
+	 * @throws Exception
+	 */
+	protected void registerToKubernetes(Properties props) throws Exception {
+		String name = props.getProperty("PLURAL") + "." + props.getProperty("GROUP");
+		String kind = props.getProperty("KIND");
+		String version = props.getProperty("GROUP") + "/" + props.getProperty("VERSION");
+
 		CustomResourceDefinition crd = getCustomResourceDefinition(name);
 		crds.put(kind, crd);
-		KubernetesDeserializer.registerCustomKind(version, kind, 
-									getCustomResourceClass(kind));
-		MixedOperation executor = (MixedOperation) customResources(
-										crds.get(VirtualMachine.class.getSimpleName()), 
-										VirtualMachine.class, 
-										VirtualMachineList.class, 
-										DoneableVirtualMachine.class).inAnyNamespace();
+		KubernetesDeserializer.registerCustomKind(version, kind, getCustomResourceClass(kind));
+		MixedOperation executor = (MixedOperation) customResources(crds.get(VirtualMachine.class.getSimpleName()),
+				VirtualMachine.class, VirtualMachineList.class, DoneableVirtualMachine.class).inAnyNamespace();
 		executors.put(kind, executor);
-    }
+	}
 
 	/**
-	 * @param conf       file name
-	 * @return           key and values
+	 * @param conf file name
+	 * @return key and values
 	 * @throws Exception the file does not exist
 	 */
 	protected Properties loadConfig(File conf) throws Exception {
@@ -103,7 +97,6 @@ public class ExtendedKubernetesClient extends DefaultKubernetesClient {
 		props.load(new FileInputStream(conf));
 		return props;
 	}
-
 
 	/**
 	 * @param crd              crd
@@ -117,66 +110,57 @@ public class ExtendedKubernetesClient extends DefaultKubernetesClient {
 			Class<? extends KubernetesResourceList> resourceList, Class<? extends Doneable> doneableRespurce) {
 		return this.customResources(crd, resourceType, resourceList, doneableRespurce);
 	}
-	
+
 	/**
-	 * @param kind       resource type
-	 * @return           resource class if the class exist
+	 * @param kind resource type
+	 * @return resource class if the class exist
 	 * @throws Exception class not found
 	 */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	protected Class<? extends KubernetesResource> getCustomResourceClass(String kind) 
-																throws Exception {
-		return (Class<? extends KubernetesResource>) 
-							Class.forName(PACKAGE + SUBPKG + kind);
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	protected Class<? extends KubernetesResource> getCustomResourceClass(String kind) throws Exception {
+		return (Class<? extends KubernetesResource>) Class.forName(PACKAGE + SUBPKG + kind);
 	}
-
 
 	/**
-	 * @param name       resource name
-	 * @return           the related CustomResourceDefinition
+	 * @param name resource name
+	 * @return the related CustomResourceDefinition
 	 */
 	protected CustomResourceDefinition getCustomResourceDefinition(String name) {
-		return this.customResourceDefinitions()
-								.withName(name).get();
+		return this.customResourceDefinitions().withName(name).get();
 	}
-  
-    
-    /**
-     * @return this files located at 'src/main/resources'
-     */
-    protected File[] getConfigs() {
-    	File res = new File(this.getClass()
-    							.getClassLoader()
-    							.getResource("")
-    							.getFile());
-    	System.out.println(res.getAbsolutePath());
-    	return res.listFiles();
-    }
-    
-    /**
-     * @param watcher watcher
-     */
-    @SuppressWarnings("unchecked")
+
+	/**
+	 * @return this files located at 'src/main/resources'
+	 */
+	protected File[] getConfigs() {
+		File res = new File(this.getClass().getClassLoader().getResource("").getFile());
+		return res.listFiles();
+	}
+
+	/**
+	 * @param watcher watcher
+	 */
+	@SuppressWarnings("unchecked")
 	public void watchVirtualMachine(Watcher<VirtualMachine> watcher) {
-    	executors.get(VirtualMachine.class.getSimpleName()).watch(watcher);
-    }
-    
-    /**
-     * @param config  config
-     * @return        ExtendedKubernetesClient 
-     * @throws Exception unable to init
-     */
-    public static ExtendedKubernetesClient defaultConfig(String config) throws Exception {
-    	return new ExtendedKubernetesClient(Serialization.unmarshal(config, Config.class));
-    }
-    
-    /**
-     * @param is      file 
-     * @return        ExtendedKubernetesClient 
-     * @throws Exception unable to init
-     */
-    public static ExtendedKubernetesClient defaultConfig(InputStream is) throws Exception {
-        return new ExtendedKubernetesClient(Serialization.unmarshal(is, Config.class));
-    }
-    
+		executors.get(VirtualMachine.class.getSimpleName()).watch(watcher);
+	}
+
+	/**
+	 * @param config config
+	 * @return ExtendedKubernetesClient
+	 * @throws Exception unable to init
+	 */
+	public static ExtendedKubernetesClient defaultConfig(String config) throws Exception {
+		return new ExtendedKubernetesClient(Serialization.unmarshal(config, Config.class));
+	}
+
+	/**
+	 * @param is file
+	 * @return ExtendedKubernetesClient
+	 * @throws Exception unable to init
+	 */
+	public static ExtendedKubernetesClient defaultConfig(InputStream is) throws Exception {
+		return new ExtendedKubernetesClient(Serialization.unmarshal(is, Config.class));
+	}
+
 }
