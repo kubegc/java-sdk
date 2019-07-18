67c1f5d0b21933cf7b2c848e70fe364f7f839997
syswu <wuheng@iscas.ac.cn>
2019-07-18 17:23:32 +0800

    example: force stop

---
+++ src/test/java/com/uit/cloud/kubernetes/ForceStopVMTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (2019, ) Institute of Software, Chinese Academy of Sciences
+ */
+package com.uit.cloud.kubernetes;
+
+import com.github.kubesys.kubernetes.ExtendedKubernetesClient;
+import com.github.kubesys.kubernetes.api.model.virtualmachine.Lifecycle.StopVMForce;
+
+/**
+ * @author wuheng@otcaix.iscas.ac.cn
+ * @since  2019/7/15
+ *
+ * This code is used to manage CustomResource's lifecycle,
+ * such as VirtualMachine
+ */
+public class ForceStopVMTest {
+	
+	
+	public static void main(String[] args) throws Exception {
+
+		ExtendedKubernetesClient client = AbstractTest.getClient();
+		boolean successful = client.virtualMachines()
+				.stopVMForce("skywind8", new StopVMForce());
+		System.out.println(successful);
+	}
+	
+}
