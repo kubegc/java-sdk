3bc10942ab12906e93211ab668081313ef574aea
742362144 <742362144@qq.com>
2019-08-23 14:16:38 +0800

    add pool

---
+++ src/test/java/com/uit/cloud/kubernetes/GetVMPoolTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (2019, ) Institute of Software, Chinese Academy of Sciences
+ */
+package com.uit.cloud.kubernetes;
+
+/**
+ * @author wuheng@otcaix.iscas.ac.cn
+ * @since  2019/5/1
+ *
+ * This code is used to manage CustomResource's lifecycle,
+ * such as VirtualMachine
+ */
+public class GetVMPoolTest {
+	
+	public static void main(String[] args) throws Exception {
+//		System.out.println(AbstractTest
+//				.getVMByName("650646e8c17a49d0b83c1c797811e066"));
+		System.out.println(AbstractTest
+				.getVMPoolByName("hello"));
+	}
+	
+}
