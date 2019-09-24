ef9dc54fcc8f56119d2bed00b11c061d58ff98f1
wuyuewen <340629194@qq.com>
2019-09-24 10:48:47 +0800

    fix description issues

--- src/main/java/com/github/kubesys/kubernetes/api/model/virtualmachinedisk/Lifecycle.java
+++ src/main/java/com/github/kubesys/kubernetes/api/model/virtualmachinedisk/Lifecycle.java
@@ -79,19 +79,19 @@ public class Lifecycle {
 		exception = AnnotationUtils.DESC_FUNCTION_EXEC)
 	protected DeleteDiskInternalSnapshot deleteDiskInternalSnapshot;
 	
-	@FunctionDescriber(shortName = "创建云盘内部快照", description = "创建云盘内部快照，" 
+	@FunctionDescriber(shortName = "创建云盘外部快照", description = "创建云盘外部快照，" 
 			+ AnnotationUtils.DESC_FUNCTION_DESC, 
 		prerequisite = AnnotationUtils.DESC_FUNCTION_VMD, 
 		exception = AnnotationUtils.DESC_FUNCTION_EXEC)
 	protected CreateDiskExternalSnapshot createDiskExternalSnapshot;
 	
-	@FunctionDescriber(shortName = "从云盘内部快照恢复", description = "从云盘内部快照恢复，" 
+	@FunctionDescriber(shortName = "从云盘外部快照恢复", description = "从云盘外部快照恢复，" 
 			+ AnnotationUtils.DESC_FUNCTION_DESC, 
 		prerequisite = AnnotationUtils.DESC_FUNCTION_VMD, 
 		exception = AnnotationUtils.DESC_FUNCTION_EXEC)
 	protected RevertDiskExternalSnapshot revertDiskExternalSnapshot;
 	
-	@FunctionDescriber(shortName = "删除云盘内部快照", description = "删除云盘内部快照，" 
+	@FunctionDescriber(shortName = "删除云盘外部快照", description = "删除云盘外部快照，" 
 			+ AnnotationUtils.DESC_FUNCTION_DESC, 
 		prerequisite = AnnotationUtils.DESC_FUNCTION_VMD, 
 		exception = AnnotationUtils.DESC_FUNCTION_EXEC)
