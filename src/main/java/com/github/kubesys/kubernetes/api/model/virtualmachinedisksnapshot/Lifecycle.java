bf540e2e3834524b4b5b9c778d0acde812782dfa
742362144 <742362144@qq.com>
2020-03-19 16:57:13 +0800

    change revert snapshot

--- src/main/java/com/github/kubesys/kubernetes/api/model/virtualmachinedisksnapshot/Lifecycle.java
+++ src/main/java/com/github/kubesys/kubernetes/api/model/virtualmachinedisksnapshot/Lifecycle.java
@@ -151,7 +151,7 @@ public class Lifecycle {
 		@Pattern(regexp = RegExpUtils.DISK_TYPE_PATTERN)
 		protected String format;
 
-		@ParameterDescriber(required = false, description = "若该云盘加载到虚拟机内（包括系统盘、数据盘），并且虚拟机处于开机状态，则需要填写该虚拟机名，否则将报错Write lock", constraint = "已存在的虚拟机名，由4-100位的数字和小写字母组成", example = "950646e8c17a49d0b83c1c797811e001")
+		@ParameterDescriber(required = false, description = "再调用该接口时，虚拟机需要处于关机状态。若该云盘加载到虚拟机内（包括系统盘、数据盘），则需要填写该虚拟机名, ，否则将报错Write lock", constraint = "已存在的虚拟机名，由4-100位的数字和小写字母组成", example = "950646e8c17a49d0b83c1c797811e001")
 		@Pattern(regexp = RegExpUtils.NAME_PATTERN)
 		protected String domain;
 
