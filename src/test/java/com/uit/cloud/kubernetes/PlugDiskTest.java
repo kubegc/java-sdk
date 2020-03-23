1a8c92eba4922007f88c986ba9cbd5d30423fb44
wuyuewen <340629194@qq.com>
2020-03-23 14:29:16 +0800

    update example

--- src/test/java/com/uit/cloud/kubernetes/PlugDiskTest.java
+++ src/test/java/com/uit/cloud/kubernetes/PlugDiskTest.java
@@ -22,7 +22,7 @@ public class PlugDiskTest {
 
 		ExtendedKubernetesClient client = AbstractTest.getClient();
 		boolean successful = client.virtualMachines()
-				.plugDisk("wywtest", getPlugBlock());
+				.plugDisk("wyw123", getPlugBlock());
 //				.plugDisk("vm003", getPlugCdrom());
 		System.out.println(successful);
 	}
@@ -46,7 +46,7 @@ public class PlugDiskTest {
 	
 	public static PlugDisk getPlugBlock() {
 		PlugDisk plugDisk = new PlugDisk();
-		plugDisk.setSource("/dev/disk/by-id/wwn-0x5000c500b5b0bd4e-part2");
+		plugDisk.setSource("/dev/disk/by-id/scsi-3600140501a77a674131403caadfc8e4c");
 		plugDisk.setTarget("sda");
 		plugDisk.setLive(true);
 		plugDisk.setConfig(true);
@@ -57,6 +57,7 @@ public class PlugDiskTest {
 		plugDisk.setRead_bytes_sec("1024000000");
 		plugDisk.setWrite_bytes_sec("1024000000");
 		plugDisk.setTargetbus("scsi");
+		plugDisk.setSourcetype("block");
 //		plugDisk.setTotal_iops_sec("40000");
 //		plugDisk.setRead_iops_sec("40000");
 //		plugDisk.setWrite_iops_sec("40000");
